429da54f1f4deb003878027bede264a21fc126e0
Anonymous <nsu.srd@gmail.com>
2016-05-15 22:50:10 +0600

    report`1`

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,25 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.stage.FileChooser;
 import javafx.stage.Popup;
 import javafx.stage.Window;
 import sample.model.algorithm.data.Parametry;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class Controller {
     @FXML private Label actiontarget;
@@ -25,10 +33,11 @@ public class Controller {
     @FXML private TextField TEXT_ENDWHILE;
     @FXML private TextField TEXT_lifeCycle;
     @FXML private LineChart realtimeChart;
-
+    @FXML private ListView reportsList;
     @FXML private Button problemSelection;
     final FileChooser fileChooser = new FileChooser();
-
+    private ArrayList reportListCheckBoxChoosed=new ArrayList<>();
+    public static final ObservableList reportsListData = FXCollections.observableArrayList();
     private XYChart.Series series;
 
 
@@ -45,6 +54,48 @@ public class Controller {
             System.out.println(file.getAbsolutePath());
         }
     }
+    @FXML public void reportDelPress(){
+        reportsListData.removeAll(reportListCheckBoxChoosed);
+    }
+    @FXML public void handleChooseReport(ActionEvent event) {
+        final Window window = ((Node)event.getTarget()).getScene().getWindow();
+        File file = fileChooser.showOpenDialog(window);
+        if (file != null) {
+            GridPane gridPane = new GridPane();
+            ColumnConstraints column1 = new ColumnConstraints(100,100,Double.MAX_VALUE);
+            column1.setHgrow(Priority.NEVER);
+            gridPane.getColumnConstraints().addAll(column1); // first column gets any extra width
+
+            CheckBox checkbox =new CheckBox();
+            checkbox.setSelected(true);
+            Label reportName = new Label(file.getAbsolutePath());
+
+
+            gridPane.add(checkbox, 1, 0);
+            gridPane.add(reportName, 0, 0);
+            reportsListData.add(gridPane);
+
+            checkbox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+                public void changed(ObservableValue<? extends Boolean> ov,
+                                    Boolean old_val, Boolean new_val) {
+                    System.out.println(file.getAbsolutePath());
+                    System.out.println(new_val);
+                    if (new_val==false) {
+                        reportListCheckBoxChoosed.add(gridPane);
+                    }
+                    if (new_val==true){
+                        reportListCheckBoxChoosed.remove(gridPane);
+                    }
+                    System.out.println(reportListCheckBoxChoosed.get(0));
+                    // reportName.setText("2312");
+                }
+            });
+
+
+
+        }
+    }
+
 
     @FXML public void derg2(){
         ggwcg.setSelected(true);
@@ -80,6 +131,7 @@ public class Controller {
         series = new XYChart.Series();
         series.setName("Скилл Доброго");
         realtimeChart.getData().add(series);
+        reportsList.setItems(reportsListData);
     }
     public void clearChart() {
         series.getData().clear();
