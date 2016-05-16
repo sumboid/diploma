371f7dcefd527d52482bafaf46301865b27bf6da
Ilya Sumbatyants <ilya.sumbatyants@xored.com>
2016-05-16 07:32:55 +0600

    report

--- src/sample/controller/report/ReportController.java
+++ src/sample/controller/report/ReportController.java
@@ -7,25 +7,45 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
+import javafx.util.Pair;
+import sample.model.report.Report;
+import sample.model.utils.FileWorker;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.*;
 
 public class ReportController {
     @FXML private ListView reportsList;
+    @FXML private LineChart reportChart;
+    @FXML private WebView reportWebView;
+
+    private WebEngine engine;
 
     private ArrayList reportListCheckBoxChoosed=new ArrayList<>();
+    private Map<GridPane, Pair<String, Report>> reportMap = new HashMap();
+    private Map<GridPane, XYChart.Series> seriesMap = new HashMap();
+
     public static final ObservableList reportsListData = FXCollections.observableArrayList();
 
     @FXML public void reportDelPress(){
+        for(Object pane: reportListCheckBoxChoosed) {
+            reportChart.getData().removeAll(seriesMap.get(pane));
+            seriesMap.remove(pane);
+            reportMap.remove(pane);
+        }
+        this.updateWebContent();
         reportsListData.removeAll(reportListCheckBoxChoosed);
     }
     @FXML public void handleChooseReport(ActionEvent event) {
@@ -33,39 +53,70 @@ public class ReportController {
         final Window window = ((Node)event.getTarget()).getScene().getWindow();
         File file = fileChooser.showOpenDialog(window);
         if (file != null) {
-            GridPane gridPane = new GridPane();
-            ColumnConstraints column1 = new ColumnConstraints(100,100,Double.MAX_VALUE);
-            column1.setHgrow(Priority.NEVER);
-            gridPane.getColumnConstraints().addAll(column1); // first column gets any extra width
+            String path = file.getAbsolutePath();
+            Report report = (Report) FileWorker.readObjectFromFile(path);
+            if (report != null) {
+                GridPane gridPane = new GridPane();
+                ColumnConstraints column1 = new ColumnConstraints(100, 100, Double.MAX_VALUE);
+                column1.setHgrow(Priority.NEVER);
+                gridPane.getColumnConstraints().addAll(column1); // first column gets any extra width
 
-            CheckBox checkbox =new CheckBox();
-            checkbox.setSelected(true);
-            Label reportName = new Label(file.getAbsolutePath());
+                CheckBox checkbox = new CheckBox();
+                checkbox.setSelected(true);
+                Label reportName = new Label(file.getAbsolutePath());
 
+                gridPane.add(checkbox, 1, 0);
+                gridPane.add(reportName, 0, 0);
+                reportsListData.add(gridPane);
+                reportMap.put(gridPane, new Pair(file.getName(), report));
+                XYChart.Series series = this.makeReportSeries(report);
+                series.setName(file.getName());
+                seriesMap.put(gridPane, series);
+                reportChart.getData().add(series);
 
-            gridPane.add(checkbox, 1, 0);
-            gridPane.add(reportName, 0, 0);
-            reportsListData.add(gridPane);
+                this.updateWebContent();
 
-            checkbox.selectedProperty().addListener(new ChangeListener<Boolean>() {
-                public void changed(ObservableValue<? extends Boolean> ov,
-                                    Boolean old_val, Boolean new_val) {
-                    System.out.println(file.getAbsolutePath());
-                    System.out.println(new_val);
-                    if (new_val==false) {
-                        reportListCheckBoxChoosed.add(gridPane);
+                checkbox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+                    public void changed(ObservableValue<? extends Boolean> ov,
+                                        Boolean old_val, Boolean new_val) {
+                        System.out.println(file.getAbsolutePath());
+                        System.out.println(new_val);
+                        if (new_val == false) {
+                            reportListCheckBoxChoosed.add(gridPane);
+                        }
+                        if (new_val == true) {
+                            reportListCheckBoxChoosed.remove(gridPane);
+                        }
+                        System.out.println(reportListCheckBoxChoosed.get(0));
+                        // reportName.setText("2312");
                     }
-                    if (new_val==true){
-                        reportListCheckBoxChoosed.remove(gridPane);
-                    }
-                    System.out.println(reportListCheckBoxChoosed.get(0));
-                    // reportName.setText("2312");
-                }
-            });
+                });
+            }
         }
     }
 
     @FXML public void initialize() {
+        engine = reportWebView.getEngine();
         reportsList.setItems(reportsListData);
+    }
+
+    @FXML private XYChart.Series makeReportSeries(Report report) {
+        XYChart.Series series = new XYChart.Series();
+        List<Double> points = report.getGrafik();
+        for(int i = 0; i < points.size(); ++i) {
+            series.getData().add(new XYChart.Data(i, points.get(i)));
+        }
+        return series;
+    }
+
+    private void updateWebContent() {
+        Map<String, Report> reportRealMap = new HashMap();
+        Collection<Pair<String, Report>> reportPairs = reportMap.values();
+
+        for (Pair<String, Report> pair : reportPairs) {
+            reportRealMap.put(pair.getKey(), pair.getValue());
+        }
+
+        engine.loadContent(HTMLbuilding.htmlBuild(reportRealMap));
     }
 }
