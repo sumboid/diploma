c82c9797a4c22d463395636c83b37005a72251b6
Ilya Sumbatyants <ilya.sumbatyants@xored.com>
2016-05-17 10:39:50 +0600

    report html refactoring

--- src/sample/controller/report/HTMLbuilding.java
+++ src/sample/controller/report/HTMLbuilding.java
@@ -8,56 +8,102 @@ import java.io.PrintWriter;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * Created by roman on 16.05.16.
- */
 public class HTMLbuilding {
-    public static String htmlBuild(Map<String, Report> reportMap, String image){
-        String resultString="";
-        int n= reportMap.size();
+    private static String genTable(Map<String, Report> reportMap) {
         Set<String> names = reportMap.keySet();
         StringBuilder builder =new StringBuilder();
-        builder.append("<html><head></head><body>");
-        builder.append("<img src=\"" + image + "\"/>");
-        builder.append("<table border=\"1\"><tr><td>ReportName</td><td>num_ants</td><td>lifeCycle</td><td>P</td><td>al</td></tr>");//<td></td>
+        builder
+                .append("<table class='report-table'>")
+                    .append("<thead>")
+                        .append("<tr>")
+                            .append("<th>ReportName</th>")
+                            .append("<th>num_ants</th>")
+                            .append("<th>lifeCycle</th>")
+                            .append("<th>P</th>")
+                            .append("<th>al</th>")
+                        .append("</tr>")
+                    .append("</thead>");
+        builder.append("<tbody>");
         for(String name : names) {
             Report report = reportMap.get(name);
-            builder.append("<tr><td>"+name+"</td>");
-            builder.append("<td>"+report.getProblem().getParams().ANTS_NUMBER+"</td>");
-            builder.append("<td>"+report.getProblem().getParams().lifeСycle+"</td>");
-            builder.append("<td>"+report.getProblem().getParams().p+"</td>");
-            builder.append("<td>"+report.getProblem().getParams().al+"</td></tr>");
+            builder
+                    .append("<tr>")
+                        .append("<td>"+name+"</td>")
+                        .append("<td>"+report.getProblem().getParams().ANTS_NUMBER+"</td>")
+                        .append("<td>"+report.getProblem().getParams().lifeСycle+"</td>")
+                        .append("<td>"+report.getProblem().getParams().p+"</td>")
+                        .append("<td>"+report.getProblem().getParams().al+"</td>")
+                    .append("</tr>");
         }
-        builder.append("</table>");
-        builder.append("</body></html>");
-        resultString=builder.toString();
-        System.out.println(resultString);
-        return resultString;
+
+        builder.append("</tbody>");
+
+        return builder.append("</table>").toString();
+    }
+
+    private static String genImage(String image) {
+        return "<img class='report-image-src' src='" + image + "'/>";
+    }
+
+    private static String genHead() {
+        StringBuilder builder =new StringBuilder();
+        builder
+                .append("<head>")
+                    .append("<style>")
+                        .append("* {")
+                            .append("font-family: sans-serif;")
+                            .append("font-size: 12px;")
+                        .append("}")
+                        .append(".report-table-container {")
+                            .append("width: 100%;")
+                        .append("}")
+                        .append(".report-table {")
+                            .append("width: 100%;")
+                        .append("}")
+                        .append("td, th {")
+                            .append("border-spacing: 0.5rem;")
+                            .append("border: 1px solid #999;")
+                            .append("height: 30px;")
+                        .append("}")
+                    .append("</style>")
+                .append("</head>");
+
+        return builder.toString();
+    }
+
+    public static String htmlBuild(Map<String, Report> reportMap, String image){
+        Set<String> names = reportMap.keySet();
+        StringBuilder builder =new StringBuilder();
+
+        builder
+                .append("<html>")
+                    .append(genHead())
+                    .append("<body>")
+                    .append(genImage(image))
+                    .append("<div class='report-table-container'>")
+                        .append(genTable(reportMap))
+                    .append("</div>")
+                    .append("</body>")
+                .append("</html>");
+
+        return builder.toString();
     }
     public static void save(String str, String path){
-      //  public static void write(String fileName, String text) {
-            //Определяем файл
-            File file = new File(path);
+        File file = new File(path);
 
-            try {
-                //проверяем, что если файл не существует то создаем его
-                if(!file.exists()){
-                    file.createNewFile();
-                }
-
-                //PrintWriter обеспечит возможности записи в файл
-                PrintWriter out = new PrintWriter(file.getAbsoluteFile());
-
-                try {
-                    //Записываем текст у файл
-                    out.print(str);
-                } finally {
-                    //После чего мы должны закрыть файл
-                    //Иначе файл не запишется
-                    out.close();
-                }
-            } catch(IOException e) {
-                throw new RuntimeException(e);
+        try {
+            if(!file.exists()){
+                file.createNewFile();
             }
+
+            PrintWriter out = new PrintWriter(file.getAbsoluteFile());
+            try {
+                out.print(str);
+            } finally {
+                out.close();
+            }
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
